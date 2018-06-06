0955165620296dc0b63a577907a34e12ce1ceda4
Nico <nicoadams000@gmail.com>
2018-06-06 10:23:25 -0500

    Environment changes (hopefully this will fix Katie's setup errors

--- data/querySDSS.py
+++ data/querySDSS.py
@@ -3,9 +3,6 @@ from urllib.error import HTTPError
 from urllib.parse import quote
 import json
 
-from astropy.coordinates import Distance, Angle
-from astropy.cosmology import WMAP9 as cosmo 
-
 # Example search URL:
 # http://skyserver.sdss.org/dr14/SkyServerWS/SearchTools/RadialSearch?ra=258.2&dec=64&radius=4.1&whichway=equatorial&limit=10&format=html&fp=none&uband=0,17&check_u=u&gband=0,15&check_g=g&whichquery=imaging
 
@@ -17,19 +14,6 @@ sqlSearchUrl = baseUrl + "SqlSearch?"
 
 def convertNoneValsTo0(hostDict):
 	return {k: 0 if hostDict[k]==None else hostDict[k] for k in hostDict}
-
-def convertRedshiftToDistance(redshift):
-	return Distance(z=redshift, cosmology=cosmo).kpc
-
-def convertOffsetToDistance(offset, redshift):
-	return convertRedshiftToDistance(redshift) * Angle(offset, unit="arcminute").rad
-
-def sortedHostsByDistance(hostList):
-	filteredHostList = filter(lambda hostDict: float(hostDict["redshift"] >= 0), hostList)
-	return sorted(
-		filteredHostList,
-		key=lambda hostDict: convertOffsetToDistance(float(hostDict['offset']), float(hostDict['redshift']))
-	)
 
 def getRadialSearchUrl(ra, dec, radius): # Radius should actually be 5 arcsec, this is 1 arcmin
 	# Returns the URL that will search SDSS at the given ra, dec, and radius
@@ -50,6 +34,15 @@ def getSDSSNearestHostsCommand(ra, dec, radius, hostNum):
 	 	    " LEFT JOIN Photoz pz ON pz.objID=p.objID"
 	 	    " ORDER BY offset"
 	 	   ).format(hostNum, ra, dec, radius)
+
+def getSDSSNearestGalaxiesCommand(ra, dec, radius, hostNum):
+	return ("SELECT TOP {}"
+		    " g.objid, g.type, g.ra, g.dec, g.u, g.g, g.r, g.i, g.z, g.err_u, g.err_g, g.err_r, g.err_i, g.err_z, n.distance AS offset, pz.z AS redshift, pz.zErr AS err_redshift"
+	 	    " FROM fGetNearbyObjEq({},{},{}) n"
+	 	    " JOIN Galaxy g ON n.objID=g.objID"
+	 	    " LEFT JOIN Photoz pz ON pz.objID=g.objID"
+	 	    " ORDER BY offset"
+		   ).format(hostNum, ra, dec, radius)
 
 def getSDSSNearestGalaxiesCommand(ra, dec, radius, hostNum):
 	return ("SELECT TOP {}"
@@ -100,11 +93,4 @@ def searchNearestGalaxyByAngle(ra, dec, radius):
 	hostList = searchNearestGalaxies(ra, dec, radius, 1)
 	if len(hostList) == 0: return None
 	return hostList[0]
-
-# Returns a dict of the nearest galaxy by est. distance, or None if there is no galaxy in the given radius
-def searchNearestGalaxyByDistance(ra, dec, radius):
-	hostList = searchNearestGalaxies(ra, dec, radius, 1000)
-	sortedHostList = sortedHostsByDistance(hostList)
-	if len(sortedHostList) == 0: return None
-	return sortedHostList[0]
 
